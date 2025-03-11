e296b0b9c083d7e41bd84901ec1ddadb5e3549ba
Олег Девяткин <oleg.deviatckin@yandex.ru>
2025-03-11 21:30:30 +0300

    Добавить интерфейс IMessageHandlerAsyncWorkflow

---
+++ sources/Deol.Lims.Abstract/IMessageHandlerAsyncWorkflow.cs
@@ -0,0 +1,10 @@
+﻿namespace Deol.Lims.Abstract
+{
+    public interface IMessageHandlerAsyncWorkflow : IMessageHandler
+    {
+        Task StartBackgroundHandlingAsync(CancellationToken cancellationToken);
+        int OutputMessageCount { get; }
+        IEnumerable<string[]>? GetNextOutputMessage();
+        void AddOutputMessage(IEnumerable<string[]> message);
+    }
+}
